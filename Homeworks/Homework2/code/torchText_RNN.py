b14df5d3a7f10d95f3785b49c6762d1893946b35
Roy <royluo@umich.edu>
2019-02-28 01:46:26 -0500

    m5

--- Homeworks/Homework2/code/torchText_RNN.py
+++ Homeworks/Homework2/code/torchText_RNN.py
@@ -52,12 +52,13 @@ class BatchGenerator:
             y = getattr(batch, self.y_field)
             yield (X,y)
             
-class model_2(nn.Module):
-    def __init__(self, vocab, embedding_dim):
-        super(model_2, self).__init__()
+class my_model(nn.Module):
+    def __init__(self, vocab, embedding_dim, n_hidden, mode):
+        super(my_model, self).__init__()
         self.vocab_size = len(vocab)
         self.embedding_dim = embedding_dim
-        
+        self.n_hidden = n_hidden
+        self.mode = mode
         '''
         # 1 is the padding number 
         self.emb = nn.Embedding(self.vocab_size, self.embedding_dim, padding_idx=1)
@@ -69,34 +70,50 @@ class model_2(nn.Module):
         #freeze embedding weight
         #self.emb.weight.requires_grad = False
         
-        
+        ##??? batch_first
+        self.lstm = nn.LSTM(self.embedding_dim, self.n_hidden, batch_first=True)
+        self.rnn = nn.RNN(self.embedding_dim, self.n_hidden, batch_first=True)
         self.fc = nn.Sequential(
-            nn.Linear(self.embedding_dim, 2),
+            nn.Linear(self.n_hidden, 2),
             nn.Sigmoid()
         )
-    
+        
+        
+    def init_hidden(self, batch_size):
+        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
+        # hidden, cell_state
+        #print("initialized hidden: ", '(1, ',batch_size, self.n_hidden,')')
+        if self.mode=='lstm':
+            return (torch.zeros(1, batch_size, self.n_hidden),torch.zeros(1, batch_size, self.n_hidden))
+        elif self.mode == 'rnn':
+            return torch.zeros(1, batch_size, self.n_hidden)
+        else:
+            print("***WRONG MODEL! ***")
     def forward(self, seq, lengths):
+        
+        batch_size = seq.shape[1]
+        #print('batch_size: ',batch_size)
+        self.hidden = self.init_hidden(batch_size)
         seq = seq.transpose(0,1)
         #print('seq_len: ',seq.shape) #3, 4: batch_size, input_dim(max sentence length in batch)
         embs = self.emb(seq) #3, 4, 5: batch_size, seq_len, hidden_dim
-        
-        '''
-        ##for later
         embs = embs.transpose(0,1)
-        print(vocab_size)
-        print(embs.shape) #4,3,5: input_dim, batch_size, hidden_dim
+        #print('embeddings:', embs.shape) #4,3,5: input_dim, batch_size, hidden_dim
         #print('lengths: ',lengths)
         
-        #embs = pack_padded_sequence(embs, lengths) # (4+4+2), 5: each length sum, hidden_dim
-        #print("padded: ",embs.data)
-        '''
-        
-        embs = embs.transpose(1,2)
-        #print('embedding after transpose: ', embs.shape) #want: 3,5,4
-        #print(embs)
-        avg_pool = torch.sum(embs, dim=2)/Variable(torch.LongTensor(lengths).view(-1,1)).float()
-        print(avg_pool.shape)
-        out = self.fc(avg_pool)
+        embs = pack_padded_sequence(embs, lengths) # (4+4+2), 5: each length sum, hidden_dim
+        #print("padded: ",embs.data.shape)
+        if self.mode == 'lstm':
+            lstm_out, self.hidden = self.lstm(embs, self.hidden)
+            #print("LSTM: ", self.hidden[0].shape,self.hidden[1].shape) # 1,64,150: seq_len, batch_size, n_hidden
+            #output, lengths = pad_packed_sequence(lstm_out)  ## need to care for loss masking
+            output = self.hidden[0]
+            #print(output.view(output.shape[1],-1).shape)
+        elif self.mode == 'rnn':
+            #print('rnn..')
+            rnn_out, self.hidden = self.rnn(embs, self.hidden)
+            output = self.hidden
+        out = self.fc(output.view(output.shape[1],-1))
         return out         
 
 
@@ -120,12 +137,13 @@ vocab = TEXT.vocab
 
 ##??????? val shuffle????
 train_iter = BucketIterator(train_ds, batch_size=64, device=-1, # if you want to use the GPU, specify the GPU number here
- train=True, sort_key=lambda x: len(x.text), # the BucketIterator needs to be told what function it should use to group the data.
- sort_within_batch=False, repeat=False, # we pass repeat=False because we want to wrap this Iterator layer.
- shuffle=True, 
+ sort_key=lambda x: len(x.text), # the BucketIterator needs to be told what function it should use to group the data.
+ sort_within_batch=True, repeat=False, # we pass repeat=False because we want to wrap this Iterator layer.
+ shuffle=True, train = True
 )
 test_iter, un_iter = Iterator.splits((test_ds, un_ds), batch_sizes=(64, 10001), device=-1, 
-                              sort=False, sort_within_batch=False, repeat=False, 
+                              sort_key=lambda x: len(x.text), sort=False, 
+                              sort_within_batch=True, repeat=False, 
                               shuffle=False)
 
 #train_iter = BucketIterator(train_ds, batch_size=3, shuffle=True,train=True)
@@ -141,12 +159,13 @@ un_batch_dl = BatchGenerator(un_iter, 'text', 'label')
 learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
 #vocab_size = len(vocab)
 embedding_dim = 100
+n_hidden = 150
 n_out = 2
-
+mode = 'rnn'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #device = torch.device('cpu')
 
-net = model_2(vocab, embedding_dim).to(device)
+net = my_model(vocab, embedding_dim, n_hidden, mode).to(device)
 criterion = nn.CrossEntropyLoss()
 #criterion = nn.NLLLoss()
 optimizer = optim.Adam(filter(lambda p: p.requires_grad, net.parameters()), lr=learning_rate)
@@ -210,7 +229,7 @@ with torch.no_grad():
         print(predicted)
         #all_predictions.append(predicted)
 print("prediction used: ", time.time() - pred_start)
-with open('data/prediction_m3.txt', 'w') as f:
+with open('data/prediction_'+mode+'.txt', 'w') as f:
     for i in predicted:
         f.write(str(int(i)))
         f.write('\n')
